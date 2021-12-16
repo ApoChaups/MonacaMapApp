b0422468e1dffb38d4e63f566088ec444475f193
ApoChaups <k20rs066@st.kyusan-u.ac.jp>
2021-12-16 10:19:57 +0900

    アプリケーションキーとクライアントキーの登録、keys.jsファイルを作り2つのキーを保護keys.jsを読み込ませるためにheadに追加

---
+++ www/js/keys.js
@@ -0,0 +1,3 @@
+// This is a JavaScript file
+var APPLICATIONKEY = "2a23d74333dd9a7f8e9df9d8b0f25bc8c0c21ee72cd9ec8d7c3f98825982da1c";
+var CLIENTKEY      = "a7793056a8a578b008645587c0752fb9ee7593bf483ab9abc63890a2eeb1c7ff";
